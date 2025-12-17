193eb6e93594bb5b07076edd63b4e0ea725e5dd9
Noumouni Traore <Traorenoumouni8@gmail.com>
2025-12-17 15:49:20 +0100

    "Creation du server node"

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
 //Contiendra le code pour notre premier server node 
+// on va créer un programme qui ecouter, enttendre des requêtes http et qui va y repondres
+
+const http = require('http'); // Importation du module http
+const server = http.createServer((req, res) => {
+  res.end('voilà la réponse du serveur');
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Serveur lancé sur le port 3000');
+});
+//pour verifer que notre serveur est entrain d'ecouter le port 3000, 
+//pour ce faire on utilise postman pour verifier avec la methode crud 
